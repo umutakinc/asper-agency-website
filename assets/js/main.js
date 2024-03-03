4a4ffb3361c2dedfff613091bcbbc103db7cf7fa
= <=>
2024-03-03 13:51:16 +0300

    swiper js eklendi.

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,3 +12,14 @@ $(document).ready(function () {
         animateNavbar();
     })
 });
+
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    loop: true,
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+});
